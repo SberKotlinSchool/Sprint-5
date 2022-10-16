968b05a9e0b314bc236e787039b7ada237090161
Valentina <mltoreg@mail.ru>
2022-10-16 15:01:22 +0300

    Sprint 4

--- serialization/src/test/kotlin/ru/sber/serialization/JsonCustomClassDeserializer.kt
+++ serialization/src/test/kotlin/ru/sber/serialization/JsonCustomClassDeserializer.kt
@@ -1,9 +1,15 @@
 package ru.sber.serialization
 
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.jupiter.api.Test
+import java.lang.IllegalArgumentException
 import kotlin.test.assertEquals
+
 
 class JsonCustomClassDeserializer {
 
@@ -12,7 +18,20 @@ class JsonCustomClassDeserializer {
         // given
         val data = """{"client": "Иванов Иван Иванович"}"""
         val objectMapper = ObjectMapper()
-
+            .registerModules(KotlinModule(), JavaTimeModule())
+            .registerModule(SimpleModule().also {
+                it.addDeserializer(Client7::class.java, object : JsonDeserializer<Client7>() {
+                    override fun deserialize(jp: JsonParser, ctxt: DeserializationContext): Client7 {
+                        val oc = jp.codec
+                        val node = oc.readTree<JsonNode>(jp)
+                        val (lastName, firstName, middleName) = node["client"].asText().split(" ").also { result ->
+                            if (result.size < 3) throw IllegalArgumentException("Некорректный объект: ${node["client"].asText()}")
+                        }
+                        return Client7(firstName, lastName, middleName)
+                    }
+                })
+            }
+            )
         // when
         val client = objectMapper.readValue<Client7>(data)
 
@@ -22,3 +41,4 @@ class JsonCustomClassDeserializer {
         assertEquals("Иванович", client.middleName)
     }
 }
+
