29fd5bbc2359437956542238739f68ee53f8bd95
PavelPopov4Kotlin <palepopov@sberbank.ru>
2022-10-30 20:15:57 +0600

    streams resplved

--- streams/src/main/kotlin/ru/sber/streams/Streams.kt
+++ streams/src/main/kotlin/ru/sber/streams/Streams.kt
@@ -39,14 +39,34 @@ fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? = this.customers
     .last()
 
 // 6. Получить самый дорогой продукт, когда-либо приобртенный покупателем.
-fun Customer.getMostExpensiveProduct(): Product? = null
+fun Customer.getMostExpensiveProduct(): Product? = this.orders
+    .flatMap { it.products }
+    .maxByOrNull { it.price }
 
 // 7. Получить соответствие в мапе: город - количество заказанных и доставленных продуктов в данный город.
-fun Shop.getNumberOfDeliveredProductByCity(): Map<City, Int> = emptyMap()
+fun Shop.getNumberOfDeliveredProductByCity(): Map<City, Int> = customers.map { it.city to it.orders
+    .filter { it.isDelivered }
+    .flatMap { it.products }
+    .count() }
+    .groupBy { it.first }
+    .map { it.key to it.value.sumOf { it.second } }.toMap()
 
 // 8. Получить соответствие в мапе: город - самый популярный продукт в городе.
-fun Shop.getMostPopularProductInCity(): Map<City, Product> = emptyMap()
+fun Shop.getMostPopularProductInCity(): Map<City, Product> = customers
+    .map { it.city to  it.orders.flatMap { it.products } }
+    .groupBy { it.first }
+    .map{ it.key to it.value.flatMap {it.second }
+        .groupBy { it }
+        .maxByOrNull { it.value.size }
+    }
+    .map { it.first to it.second!!.key }.toMap()
 
 // 9. Получить набор товаров, которые заказывали все покупатели.
-fun Shop.getProductsOrderedByAll(): Set<Product> = emptySet()
+fun Shop.getProductsOrderedByAll(): Set<Product> = customers
+    .flatMap { it.orders.flatMap { it.products } }
+    .map { product -> product to customers.map { it.orders.filter { it.products.contains(product) } } }
+    .filter { it.second.minOf { it.size } != 0 }
+    .groupBy { it.first }
+    .map { it.key }
+    .toSet()
 
