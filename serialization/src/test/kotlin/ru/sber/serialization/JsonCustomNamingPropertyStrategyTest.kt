4459655ac48c5e6361e0983096c109713bd6646d
Maria Starikova <starikova.mariya.v@gmail.com>
2022-10-31 17:18:13 +0300

    add streams,serialization

--- serialization/src/test/kotlin/ru/sber/serialization/JsonCustomNamingPropertyStrategyTest.kt
+++ serialization/src/test/kotlin/ru/sber/serialization/JsonCustomNamingPropertyStrategyTest.kt
@@ -1,6 +1,11 @@
 package ru.sber.serialization
 
+import com.fasterxml.jackson.databind.MapperFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
@@ -11,9 +16,12 @@ class JsonCustomNamingPropertyStrategyTest {
     @Test
     fun `Кастомная стратегия десериализации`() {
         // given
-        val data =
-            """{"FIRSTNAME": "Иван", "LASTNAME": "Иванов", "MIDDLENAME": "Иванович", "PASSPORTNUMBER": "123456", "PASSPORTSERIAL": "1234", "BIRTHDATE": "1990-01-01"}"""
+                val data =
+            """{"FIRSTNAME": "Иван", "LASTNAME": "Иванов", "MIDDLENAME": "Иванович", "PASSPORTNUMBER": "123456", "PASSPORTSERIAL": "1234","BIRTHDATE": "1990-01-01"}"""
+
         val objectMapper = ObjectMapper()
+            .registerModules(KotlinModule(), JavaTimeModule())
+            .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
 
         // when
         val client = objectMapper.readValue<Client1>(data)
