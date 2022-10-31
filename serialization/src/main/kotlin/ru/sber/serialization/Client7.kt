8df799ae7a83aeafb56bf85f241196f37f3c9abb
Даниил Тетерин <theweaklessone@yandex.ru>
2022-10-31 18:06:23 +0300

    All done

--- serialization/src/main/kotlin/ru/sber/serialization/Client7.kt
+++ serialization/src/main/kotlin/ru/sber/serialization/Client7.kt
@@ -1,5 +1,30 @@
 package ru.sber.serialization
 
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+
+private class SingleLineDeserializer: JsonDeserializer<Client7>() {
+    private val fieldName = "client"
+
+    override fun deserialize(p0: JsonParser?, p1: DeserializationContext?): Client7 {
+        val node = p0?.readValueAsTree<JsonNode>()
+        val result = node?.get(fieldName)
+                .toString()
+                .filter { it.isLetter() || it.isWhitespace() }
+                .split(" ")
+
+        return Client7(
+                firstName = result[1],
+                lastName = result[0],
+                middleName = result[2]
+        )
+    }
+}
+
+@JsonDeserialize(using = SingleLineDeserializer::class)
 data class Client7(
     val firstName: String,
     val lastName: String,
--- streams/src/main/kotlin/ru/sber/streams/Streams.kt
+++ streams/src/main/kotlin/ru/sber/streams/Streams.kt
@@ -2,33 +2,64 @@ package ru.sber.streams
 
 
 // 1. Используя withIndex() посчитать сумму элементов листа, индекс которых кратен 3. (нулевой индекс тоже входит)
-fun getSumWithIndexDivisibleByThree(list: List<Long>): Long {
-    return 0
-}
+fun getSumWithIndexDivisibleByThree(list: List<Long>) =
+        list
+            .filterIndexed { index, _ -> index % 3 == 0 }
+            .sum()
 
 // 2. Используя функцию generateSequence() создать последовательность, возвращающую числа Фибоначчи.
-fun generateFibonacciSequence(): Sequence<Int> {
-    return emptySequence()
-}
+fun generateFibonacciSequence(): Sequence<Int> = generateSequence(Pair(0, 1)) { Pair(it.second, it.first + it.second) }.map { it.first }
 
 // 3. Получить города, в которых есть покупатели.
-fun Shop.getCustomersCities(): Set<City> = emptySet()
+fun Shop.getCustomersCities(): Set<City> = customers.map { it.city }.toSet()
 
 // 4. Получить все когда-либо заказанные продукты.
-fun Shop.allOrderedProducts(): Set<Product> = emptySet()
+fun Shop.allOrderedProducts(): Set<Product> =
+        customers
+            .flatMap { customer ->
+                customer.orders.flatMap { order ->
+                    order.products
+                }
+            }
+            .toSet()
 
 // 5. Получить покупателя, который сделал больше всего заказов.
-fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? = null
+fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? =
+        customers.maxByOrNull { it.orders.size }
 
 // 6. Получить самый дорогой продукт, когда-либо приобртенный покупателем.
-fun Customer.getMostExpensiveProduct(): Product? = null
+fun Customer.getMostExpensiveProduct(): Product? =
+        orders
+                .map { it.products }
+                .flatten()
+                .maxByOrNull { it.price }
 
 // 7. Получить соответствие в мапе: город - количество заказанных и доставленных продуктов в данный город.
-fun Shop.getNumberOfDeliveredProductByCity(): Map<City, Int> = emptyMap()
+fun Shop.getNumberOfDeliveredProductByCity(): Map<City, Int> = customers
+        .groupBy { it.city }
+        .mapValues {
+            it.value.sumOf { customer ->
+                customer.orders
+                        .filter { order -> order.isDelivered }
+                        .sumOf { order -> order.products.size }
+            }
+        }
+
 
 // 8. Получить соответствие в мапе: город - самый популярный продукт в городе.
-fun Shop.getMostPopularProductInCity(): Map<City, Product> = emptyMap()
+fun Shop.getMostPopularProductInCity(): Map<City, Product> = customers
+        .groupBy { it.city }
+        .mapValues {
+            it.value
+                    .flatMap { customer -> customer.orders }
+                    .flatMap { order -> order.products }
+                    .groupBy { product -> product }
+                    .maxByOrNull { productMap -> productMap.value.size }!!
+                    .key
+        }
 
 // 9. Получить набор товаров, которые заказывали все покупатели.
-fun Shop.getProductsOrderedByAll(): Set<Product> = emptySet()
-
+fun Shop.getProductsOrderedByAll(): Set<Product> = customers
+        .fold(allOrderedProducts()) { result, customer ->
+            result.intersect(customer.orders.flatMap { it.products }.toSet())
+        }
