5bb461c61ebef3295036fb1a8e47c69c3419c14d
Gaponchuk Alexey <aggaponchuk@gmail.com>
2022-10-31 10:58:33 +0300

    JsonCustomNamingPropertyStrategyTest fix

--- serialization/src/main/kotlin/ru/sber/serialization/Client1.kt
+++ serialization/src/main/kotlin/ru/sber/serialization/Client1.kt
@@ -1,12 +1,19 @@
 package ru.sber.serialization
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.LocalDate
 
 data class Client1(
+    @JsonProperty("FIRSTNAME")
     val firstName: String,
+    @JsonProperty("LASTNAME")
     val lastName: String,
+    @JsonProperty("MIDDLENAME")
     val middleName: String?,
+    @JsonProperty("PASSPORTNUMBER")
     val passportNumber: String,
+    @JsonProperty("PASSPORTSERIAL")
     val passportSerial: String,
+    @JsonProperty("BIRTHDATE")
     val birthDate: LocalDate
 )
--- serialization/src/test/kotlin/ru/sber/serialization/JsonCustomClassDeserializer.kt
+++ serialization/src/test/kotlin/ru/sber/serialization/JsonCustomClassDeserializer.kt
@@ -1,6 +1,9 @@
 package ru.sber.serialization
 
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
@@ -12,6 +15,11 @@ class JsonCustomClassDeserializer {
         // given
         val data = """{"client": "Иванов Иван Иванович"}"""
         val objectMapper = ObjectMapper()
+            .registerModule(KotlinModule())
+            .configure(DeserializationFeature.UNWRAP_ROOT_VALUE,false)
+            .enable(SerializationFeature.INDENT_OUTPUT)
+
+        //val initialObject = Client7(firstName = "Иван", lastName = "Иванов", middleName = "Иванович")
 
         // when
         val client = objectMapper.readValue<Client7>(data)
