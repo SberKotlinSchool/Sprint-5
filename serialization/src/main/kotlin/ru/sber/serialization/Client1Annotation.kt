29af2e378068d57f6fb3fa33afa58870dd17188c
Мосягин Стaнислав Владимирович <Mosyagin.S.V@sberbank.ru>
2022-11-15 22:37:30 +0500

    serialization

--- serialization/src/main/kotlin/ru/sber/serialization/Client1Annotation.kt
+++ serialization/src/main/kotlin/ru/sber/serialization/Client1Annotation.kt
@@ -1,14 +1,14 @@
 package ru.sber.serialization
 
-import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import java.time.LocalDate
 
-data class Client3(
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Client1Annotation(
     val firstName: String,
     val lastName: String,
     val middleName: String?,
     val passportNumber: String,
     val passportSerial: String,
-    @JsonFormat(pattern = "dd-MM-yyyy")
     val birthDate: LocalDate
 )
--- serialization/src/test/kotlin/ru/sber/serialization/JsonCustomClassDeserializer.kt
+++ serialization/src/test/kotlin/ru/sber/serialization/JsonCustomClassDeserializer.kt
@@ -1,6 +1,12 @@
 package ru.sber.serialization
 
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.module.kotlin.kotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
@@ -12,6 +18,17 @@ class JsonCustomClassDeserializer {
         // given
         val data = """{"client": "Иванов Иван Иванович"}"""
         val objectMapper = ObjectMapper()
+            .registerModules(kotlinModule())
+            .registerModule(SimpleModule().also {
+                it.addDeserializer(Client7::class.java, object : JsonDeserializer<Client7>() {
+                    override fun deserialize(parser: JsonParser, dzc: DeserializationContext?): Client7 {
+                        val node = parser.codec.readTree<JsonNode>(parser)
+                        val (lastName, firstName, middleName) = node["client"].asText().split(" ")
+                        return Client7(firstName, lastName, middleName)
+                    }
+                }
+                )
+            })
 
         // when
         val client = objectMapper.readValue<Client7>(data)
