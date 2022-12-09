d3ca6fa22d7b781e17e81b09c4326dc0f9855c72
Anna Popova <anna.popova201909@gmail.com>
2022-12-09 20:10:49 +0300

    Streams 8th is  ready

--- streams/src/main/kotlin/ru/sber/streams/Streams.kt
+++ streams/src/main/kotlin/ru/sber/streams/Streams.kt
@@ -22,18 +22,23 @@ fun Shop.getCustomersCities(): Set<City> = customers.map { it.city }.toSet()
 // 4. Получить все когда-либо заказанные продукты.
 fun Shop.allOrderedProducts(): Set<Product> = customers.flatMap { it.orders.flatMap { it.products } }.toSet()
 
-// 5. Получить покупателя, который сделал больше всего заказов.
-fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? = null
+//5. Получить покупателя, который сделал больше всего заказов.
+fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? = customers.maxBy { it.orders.size }
 
-// 6. Получить самый дорогой продукт, когда-либо приобртенный покупателем.
-fun Customer.getMostExpensiveProduct(): Product? = null
+// 6. Получить самый дорогой продукт, когда-либо приобретенный покупателем.
+fun Customer.getMostExpensiveProduct(): Product? = orders.filter { it.isDelivered }.flatMap { it.products }.maxBy { it.price }
 
-// 7. Получить соответствие в мапе: город - количество заказанных и доставленных продуктов в данный город.
-fun Shop.getNumberOfDeliveredProductByCity(): Map<City, Int> = emptyMap()
+// TODO7. Получить соответствие в мапе: город - количество заказанных и доставленных продуктов в данный город.
+fun Shop.getNumberOfDeliveredProductByCity(): Map<City, Int> = customers.groupBy { it.city }.mapValues { it.value.sumOf { it.orders.filter { it.isDelivered }.sumOf { it.products.size } } }
 
 // 8. Получить соответствие в мапе: город - самый популярный продукт в городе.
-fun Shop.getMostPopularProductInCity(): Map<City, Product> = emptyMap()
+fun Shop.getMostPopularProductInCity(): Map<City, Product?> {
+    val deliveredProducts: Map<City, List<Product>> = customers.groupBy { it.city }.mapValues { it.value.flatMap { it.orders.filter { it.isDelivered } }.flatMap { it.products } }
+
+    val mapValues2: Map<City, Map.Entry<Product, Int>?> = deliveredProducts.mapValues { it.value.groupingBy { it }.eachCount().filter { it.value > 1 }.maxBy { it.value } }
+
+    return mapValues2.mapValues { it.value?.key }
+}
 
 // 9. Получить набор товаров, которые заказывали все покупатели.
-fun Shop.getProductsOrderedByAll(): Set<Product> = emptySet()
-
+fun Shop.getProductsOrderedByAll(): Set<Product> =  customers.flatMap { it.orders }.flatMap { it.products }.toSet()
