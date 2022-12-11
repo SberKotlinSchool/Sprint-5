f723582dcceac79243b6f683f320e97f9d973ed7
16939749 <astafyev.a.igor@sberbank.ru>
2022-12-11 15:48:38 +0300

    выполнены задания по Stream API

--- streams/src/main/kotlin/ru/sber/streams/Streams.kt
+++ streams/src/main/kotlin/ru/sber/streams/Streams.kt
@@ -1,6 +1,5 @@
 package ru.sber.streams
 
-
 // 1. Используя withIndex() посчитать сумму элементов листа, индекс которых кратен 3. (нулевой индекс тоже входит)
 fun getSumWithIndexDivisibleByThree(list: List<Long>): Long = list
     .withIndex()
@@ -43,8 +42,26 @@ fun Shop.getNumberOfDeliveredProductByCity(): Map<City, Int> = customers
     }
 
 // 8. Получить соответствие в мапе: город - самый популярный продукт в городе.
-fun Shop.getMostPopularProductInCity(): Map<City, Product> = emptyMap()
+fun Shop.getMostPopularProductInCity(): Map<City, Product> = customers
+    .groupBy { it.city }
+    .mapValues { entry ->
+        entry.value
+            .flatMap { customer ->
+                customer.orders
+                    .flatMap { order -> order.products }
+            }
+            .groupBy { it }
+            .mapValues { it.value.count() }
+            .maxWithOrNull(Comparator.comparing { it.value })?.key!!
+    }
 
 // 9. Получить набор товаров, которые заказывали все покупатели.
-fun Shop.getProductsOrderedByAll(): Set<Product> = emptySet()
+fun Shop.getProductsOrderedByAll(): Set<Product> = customers
+    .fold(allOrderedProducts()) { value, customer ->
+        value.intersect(customer.orders
+            .flatMap { it.products }
+            .toSet()
+        )
+    }
+
 
