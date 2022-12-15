f982e8ceabaf27e6215cfdabf9fdf5918fc05c6b
Dubovkin Maxim <dubovkin.m@yandex.ru>
2022-12-16 00:32:28 +0500

    serialization

--- serialization/src/main/kotlin/ru/sber/serialization/Client1.kt
+++ serialization/src/main/kotlin/ru/sber/serialization/Client1.kt
@@ -1,12 +1,22 @@
 package ru.sber.serialization
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.LocalDate
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Client1(
+    @JsonProperty("firstName")
     val firstName: String,
+    @JsonProperty("lastName")
     val lastName: String,
+    @JsonProperty("middleName")
     val middleName: String?,
+    @JsonProperty("passportNumber")
     val passportNumber: String,
+    @JsonProperty("passportSerial")
     val passportSerial: String,
+    @JsonProperty("birthDate")
     val birthDate: LocalDate
 )
