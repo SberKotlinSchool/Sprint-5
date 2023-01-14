6a5e8bb9a7a4f510ea7f4508453835a5484932fc
SlumdogProgrammer <bolotin99@inbox.ru>
2023-01-15 02:27:48 +0300

    serialization done

--- serialization/src/main/kotlin/ru/sber/serialization/Client7.kt
+++ serialization/src/main/kotlin/ru/sber/serialization/Client7.kt
@@ -1,7 +1,22 @@
 package ru.sber.serialization
 
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+
+@JsonDeserialize(using = Client7Deserializer::class)
 data class Client7(
     val firstName: String,
     val lastName: String,
     val middleName: String,
 )
+
+class Client7Deserializer : JsonDeserializer<Client7>() {
+    override fun deserialize(jsonParser: JsonParser, dc: DeserializationContext?): Client7 {
+        val node = jsonParser.readValueAsTree<JsonNode>()
+        val client = node["client"].asText().split(" ")
+        return Client7(client[1], client[0], client[2])
+    }
+}
