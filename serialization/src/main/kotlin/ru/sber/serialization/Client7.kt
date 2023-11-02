5ebc9ae106aeb54ff15ef7a742cd612194f81ca3
Slavan99 <99ken@mail.ru>
2023-11-02 22:23:15 +0300

    Sprint 4 - serialization

--- serialization/src/main/kotlin/ru/sber/serialization/Client7.kt
+++ serialization/src/main/kotlin/ru/sber/serialization/Client7.kt
@@ -1,7 +1,23 @@
 package ru.sber.serialization
 
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+
+@JsonDeserialize(using = Client7Deserializer::class)
 data class Client7(
     val firstName: String,
     val lastName: String,
     val middleName: String,
 )
+
+class Client7Deserializer : JsonDeserializer<Client7>() {
+    override fun deserialize(jp: JsonParser, context: DeserializationContext): Client7 {
+        val node = jp.readValueAsTree<JsonNode>()
+        val client = node.get("client").asText()
+        val (lastName, firstName, middleName) = client.split(" ")
+        return Client7(firstName, lastName, middleName)
+    }
+}
