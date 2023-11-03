c46a669313eab7a254daf1c9adc0a3a1471b0a3b
Stanislav Shamsheev <sashamsheev@sberbank.ru>
2023-11-03 23:27:12 +0700

    sprint 4. finish

--- streams/src/main/kotlin/ru/sber/streams/Streams.kt
+++ streams/src/main/kotlin/ru/sber/streams/Streams.kt
@@ -2,33 +2,83 @@ package ru.sber.streams
 
 
 // 1. Используя withIndex() посчитать сумму элементов листа, индекс которых кратен 3. (нулевой индекс тоже входит)
-fun getSumWithIndexDivisibleByThree(list: List<Long>): Long {
-    return 0
-}
+fun getSumWithIndexDivisibleByThree(list: List<Long>): Long =
+    list
+        .withIndex()
+        .filter { indexedValue -> indexedValue.index % 3 == 0 }
+        .sumOf { indexedValue -> indexedValue.value }
 
 // 2. Используя функцию generateSequence() создать последовательность, возвращающую числа Фибоначчи.
-fun generateFibonacciSequence(): Sequence<Int> {
-    return emptySequence()
-}
+fun generateFibonacciSequence(): Sequence<Int> =
+    generateSequence(0 to 1) { it.second to (it.first + it.second) }
+        .map { it.first }
 
 // 3. Получить города, в которых есть покупатели.
-fun Shop.getCustomersCities(): Set<City> = emptySet()
+fun Shop.getCustomersCities(): Set<City> =
+    customers
+        .map(Customer::city)
+        .toSet()
 
 // 4. Получить все когда-либо заказанные продукты.
-fun Shop.allOrderedProducts(): Set<Product> = emptySet()
+fun Shop.allOrderedProducts(): Set<Product> =
+    customers
+        .flatMap(Customer::orders)
+        .flatMap(Order::products)
+        .toSet()
 
 // 5. Получить покупателя, который сделал больше всего заказов.
-fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? = null
+fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? =
+    customers
+        .maxByOrNull { customer -> customer.orders.size }
 
 // 6. Получить самый дорогой продукт, когда-либо приобртенный покупателем.
-fun Customer.getMostExpensiveProduct(): Product? = null
+fun Customer.getMostExpensiveProduct(): Product? =
+    orders
+        .flatMap(Order::products)
+        .maxByOrNull(Product::price)
 
 // 7. Получить соответствие в мапе: город - количество заказанных и доставленных продуктов в данный город.
-fun Shop.getNumberOfDeliveredProductByCity(): Map<City, Int> = emptyMap()
+fun Shop.getNumberOfDeliveredProductByCity(): Map<City, Int> =
+    customers.groupBy(
+        keySelector = Customer::city,
+        valueTransform = { customer ->
+            customer.orders
+                .filter(Order::isDelivered)
+                .flatMap(Order::products)
+                .count()
+        })
+        .mapValues { listProductsCountForCity ->
+            listProductsCountForCity.value.sum()
+        }
 
 // 8. Получить соответствие в мапе: город - самый популярный продукт в городе.
-fun Shop.getMostPopularProductInCity(): Map<City, Product> = emptyMap()
+fun Shop.getMostPopularProductInCity(): Map<City, Product> =
+    customers.groupBy(
+        keySelector = Customer::city,
+        valueTransform = Customer::orders
+    )
+        .mapValues { listOrdersForCity ->
+            listOrdersForCity.value
+                .flatten()
+                .flatMap(Order::products)
+                .groupingBy { it }
+                .eachCount()
+                .maxByOrNull(Map.Entry<Product, Int>::value)!!
+                .key
+        }
 
 // 9. Получить набор товаров, которые заказывали все покупатели.
-fun Shop.getProductsOrderedByAll(): Set<Product> = emptySet()
+fun Shop.getProductsOrderedByAll(): Set<Product> =
+    customers.groupBy(
+        keySelector = Customer::name,
+        valueTransform = Customer::orders
+    )
+        .mapValues { listOrdersForCustomer ->
+            listOrdersForCustomer.value.flatten()
+                .flatMap(Order::products)
+                .toSet()
+        }
+        .map { productsSetForCustomer -> productsSetForCustomer.value.toMutableList() }
+        .reduce { acc, next -> acc.apply { retainAll(next) } }
+        .toSet()
 
