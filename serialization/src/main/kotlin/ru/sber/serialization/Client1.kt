80b37b8af66eb7fd7d0ada167ba5a63586c1fe9d
Anton Zhukov <devzhukov@gmail.com>
2023-11-08 21:33:57 +0300

    Sprint-5 streams + serialization

--- serialization/src/main/kotlin/ru/sber/serialization/Client1.kt
+++ serialization/src/main/kotlin/ru/sber/serialization/Client1.kt
@@ -1,7 +1,8 @@
 package ru.sber.serialization
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import java.time.LocalDate
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Client1(
     val firstName: String,
     val lastName: String,
