2d780f189c529174dc585637e00510f8a1525eed
Firebat <flamebat@mail.ru>
2023-11-08 20:57:59 +0300

    Sprint 5

--- serialization/src/test/kotlin/ru/sber/serialization/JsonCustomClassDeserializer.kt
+++ serialization/src/test/kotlin/ru/sber/serialization/JsonCustomClassDeserializer.kt
@@ -1,6 +1,12 @@
 package ru.sber.serialization
 
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
@@ -12,6 +18,9 @@ class JsonCustomClassDeserializer {
         // given
         val data = """{"client": "Иванов Иван Иванович"}"""
         val objectMapper = ObjectMapper()
+            .registerModules(KotlinModule(), SimpleModule().also {
+                it.addDeserializer(Client7::class.java, Client7Deserializer())
+            })
 
         // when
         val client = objectMapper.readValue<Client7>(data)
@@ -20,5 +29,17 @@ class JsonCustomClassDeserializer {
         assertEquals("Иван", client.firstName)
         assertEquals("Иванов", client.lastName)
         assertEquals("Иванович", client.middleName)
+    }
+}
+
+class Client7Deserializer : JsonDeserializer<Client7>() {
+    override fun deserialize(jsonParser: JsonParser, context: DeserializationContext): Client7 {
+        val jsonNode = jsonParser.codec.readTree<JsonNode>(jsonParser)
+        val fields = jsonNode.get("client").textValue().split(" ")
+        val lastName = fields.getOrElse(0) { "" }
+        val firstName = fields.getOrElse(1) { "" }
+        val middleName = fields.getOrElse(2) { "" }
+
+        return Client7(firstName, lastName, middleName)
     }
 }
--- streams/src/main/kotlin/ru/sber/streams/Streams.kt
+++ streams/src/main/kotlin/ru/sber/streams/Streams.kt
@@ -3,32 +3,42 @@ package ru.sber.streams
 
 // 1. Используя withIndex() посчитать сумму элементов листа, индекс которых кратен 3. (нулевой индекс тоже входит)
 fun getSumWithIndexDivisibleByThree(list: List<Long>): Long {
-    return 0
+    return list.withIndex().filter { it.index % 3 == 0 }.sumOf { it.value }
 }
 
 // 2. Используя функцию generateSequence() создать последовательность, возвращающую числа Фибоначчи.
 fun generateFibonacciSequence(): Sequence<Int> {
-    return emptySequence()
+    return generateSequence(Pair(0, 1)) { Pair(it.second, it.first + it.second) }.map { it.first }
 }
 
 // 3. Получить города, в которых есть покупатели.
-fun Shop.getCustomersCities(): Set<City> = emptySet()
+fun Shop.getCustomersCities(): Set<City> = customers.map { it.city }.toSet()
 
 // 4. Получить все когда-либо заказанные продукты.
-fun Shop.allOrderedProducts(): Set<Product> = emptySet()
+fun Shop.allOrderedProducts(): Set<Product> = customers.flatMap { it.orders }.flatMap { it.products }.toSet()
 
 // 5. Получить покупателя, который сделал больше всего заказов.
-fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? = null
+fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? = customers.maxByOrNull { it.orders.size }
 
 // 6. Получить самый дорогой продукт, когда-либо приобртенный покупателем.
-fun Customer.getMostExpensiveProduct(): Product? = null
+fun Customer.getMostExpensiveProduct(): Product? = orders.flatMap { it.products }.maxByOrNull { it.price }
 
+// Здесь и далее прописал явно названия некоторых сущностей вместо it
 // 7. Получить соответствие в мапе: город - количество заказанных и доставленных продуктов в данный город.
-fun Shop.getNumberOfDeliveredProductByCity(): Map<City, Int> = emptyMap()
+fun Shop.getNumberOfDeliveredProductByCity(): Map<City, Int> = customers.groupBy { it.city }.mapValues { entry ->
+    entry.value.sumOf { customer ->
+        customer.orders.filter { order -> order.isDelivered }.sumOf { order -> order.products.size }
+    }
+}
 
 // 8. Получить соответствие в мапе: город - самый популярный продукт в городе.
-fun Shop.getMostPopularProductInCity(): Map<City, Product> = emptyMap()
+fun Shop.getMostPopularProductInCity(): Map<City, Product> = customers.groupBy { it.city }.mapValues { entry ->
+    entry.value.flatMap { customer -> customer.orders }.flatMap { order -> order.products }
+        .groupBy { product -> product }.maxByOrNull { productOrdersEntry -> productOrdersEntry.value.size }!!.key
+}
 
+// С помощью стрима получаю сет, в котором лежат листы с товарами каждого заказа, затем сравниваю попарно его элементы в поисках пересечений
 // 9. Получить набор товаров, которые заказывали все покупатели.
-fun Shop.getProductsOrderedByAll(): Set<Product> = emptySet()
-
+fun Shop.getProductsOrderedByAll(): Set<Product> =
+    this.customers.map { customer -> customer.orders.flatMap { order -> order.products }.toSet() }
+        .reduce { result, products -> result.intersect(products) }
