5a71e3528809ff531223d369f8b87c112d82d561
popla <poplavski79@gmail.com>
2023-11-08 23:03:32 +0700

    Sprint 4

--- serialization/src/test/kotlin/ru/sber/serialization/JsonCustomClassDeserializer.kt
+++ serialization/src/test/kotlin/ru/sber/serialization/JsonCustomClassDeserializer.kt
@@ -1,8 +1,16 @@
 package ru.sber.serialization
 
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.core.TreeNode
+import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.databind.node.TextNode
+import com.fasterxml.jackson.module.kotlin.kotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
+import io.mockk.InternalPlatformDsl.toStr
 import org.junit.jupiter.api.Test
+import org.junit.platform.engine.support.hierarchical.Node
 import kotlin.test.assertEquals
 
 class JsonCustomClassDeserializer {
@@ -11,7 +19,8 @@ class JsonCustomClassDeserializer {
     fun `Нобходимо десериализовать данные в класс`() {
         // given
         val data = """{"client": "Иванов Иван Иванович"}"""
-        val objectMapper = ObjectMapper()
+        val objectMapper = ObjectMapper().registerModules(kotlinModule(),
+            SimpleModule().addDeserializer(Client7::class.java,  Client7Deserializer() ))
 
         // when
         val client = objectMapper.readValue<Client7>(data)
@@ -20,5 +29,14 @@ class JsonCustomClassDeserializer {
         assertEquals("Иван", client.firstName)
         assertEquals("Иванов", client.lastName)
         assertEquals("Иванович", client.middleName)
+    }
+}
+
+class Client7Deserializer: JsonDeserializer<Client7>() {
+
+    override fun deserialize(p0: JsonParser, p1: DeserializationContext): Client7 {
+      val oNode : ObjectNode = p0.readValueAsTree()
+        val (lastName, firstName, middleName) = oNode.get("client").asText().split(" ")
+        return Client7(firstName, lastName, middleName)
     }
 }
