05829cc47ec18d73ba29f5e85995915bf0f69f95
DOKL57 <daniil2705@gmail.com>
2023-11-11 18:21:04 +0300

    Sprint-5 serialization/deserialization

--- serialization/src/main/java/ru/sber/serialization/Client3.kt
+++ serialization/src/main/java/ru/sber/serialization/Client3.kt
@@ -1,12 +1,14 @@
 package ru.sber.serialization
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDate
 
-data class Client2(
+data class Client3(
     val firstName: String,
     val lastName: String,
     val middleName: String?,
     val passportNumber: String,
     val passportSerial: String,
+    @JsonFormat(pattern = "dd-MM-yyyy")
     val birthDate: LocalDate
 )
--- serialization/src/main/kotlin/ru/sber/serialization/Client7.kt
+++
@@ -1,7 +0,0 @@
-package ru.sber.serialization
-
-data class Client7(
-    val firstName: String,
-    val lastName: String,
-    val middleName: String,
-)
