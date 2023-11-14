c49bd4453aad9d5e867970e664681ac3b820eab6
denisov-ie <denisov.ilia@gmail.com>
2023-11-15 02:29:39 +0300

    Прошу проверить и зааппрувить

--- serialization/src/test/kotlin/ru/sber/serialization/JsonCustomClassDeserializer.kt
+++ serialization/src/test/kotlin/ru/sber/serialization/JsonCustomClassDeserializer.kt
@@ -1,6 +1,12 @@
 package ru.sber.serialization
 
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
@@ -8,10 +14,10 @@ import kotlin.test.assertEquals
 class JsonCustomClassDeserializer {
 
     @Test
-    fun `Нобходимо десериализовать данные в класс`() {
+    fun `Необходимо десериализовать данные в класс`() {
         // given
         val data = """{"client": "Иванов Иван Иванович"}"""
-        val objectMapper = ObjectMapper()
+        val objectMapper = ObjectMapper().registerModules(KotlinModule().addDeserializer(Client7::class.java, FioSplitterDeserializer()))
 
         // when
         val client = objectMapper.readValue<Client7>(data)
@@ -21,4 +27,21 @@ class JsonCustomClassDeserializer {
         assertEquals("Иванов", client.lastName)
         assertEquals("Иванович", client.middleName)
     }
+}
+
+class FioSplitterDeserializer: JsonDeserializer<Client7>() {
+    override fun deserialize(parser: JsonParser, context: DeserializationContext): Client7 {
+        val fioRegexp = Regex("([А-ЯЁ][а-яё]+[\\-\\s]?){3,}")
+        val propertyException = UnrecognizedPropertyException.from(context, "Некорректный формат данных, ожидается поле 'client' с ФИО, разделенными пробелами")
+
+        val node = parser.readValueAsTree<JsonNode>()
+        val client = node?.get("client")?.textValue() ?: throw propertyException
+        if (!fioRegexp.matches(client)) {
+            throw propertyException
+        }
+        val fioArray = client.split(" ")
+
+        return Client7(fioArray[1], fioArray[0], fioArray[2])
+    }
+
 }
--- streams/src/main/kotlin/ru/sber/streams/Streams.kt
+++ streams/src/main/kotlin/ru/sber/streams/Streams.kt
@@ -3,32 +3,58 @@ package ru.sber.streams
 
 // 1. Используя withIndex() посчитать сумму элементов листа, индекс которых кратен 3. (нулевой индекс тоже входит)
 fun getSumWithIndexDivisibleByThree(list: List<Long>): Long {
-    return 0
+    return list.withIndex().filter { it.index % 3 == 0 }.sumOf { it.value }
 }
 
 // 2. Используя функцию generateSequence() создать последовательность, возвращающую числа Фибоначчи.
 fun generateFibonacciSequence(): Sequence<Int> {
-    return emptySequence()
+    return generateSequence(Pair(0, 1)) { Pair(it.second, it.first + it.second) }.map { it.first }
 }
 
 // 3. Получить города, в которых есть покупатели.
-fun Shop.getCustomersCities(): Set<City> = emptySet()
+fun Shop.getCustomersCities(): Set<City> {
+    return customers.map { it.city }.toSet()
+}
 
 // 4. Получить все когда-либо заказанные продукты.
-fun Shop.allOrderedProducts(): Set<Product> = emptySet()
+fun Shop.allOrderedProducts(): Set<Product> {
+    return customers.flatMap { it.orders }.flatMap { it.products }.toSet()
+}
 
 // 5. Получить покупателя, который сделал больше всего заказов.
-fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? = null
+fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? {
+    return customers.maxByOrNull { it.orders.size }
+}
 
 // 6. Получить самый дорогой продукт, когда-либо приобртенный покупателем.
-fun Customer.getMostExpensiveProduct(): Product? = null
+fun Customer.getMostExpensiveProduct(): Product? {
+    return orders.flatMap { it.products }.maxByOrNull { it.price }
+}
 
 // 7. Получить соответствие в мапе: город - количество заказанных и доставленных продуктов в данный город.
-fun Shop.getNumberOfDeliveredProductByCity(): Map<City, Int> = emptyMap()
+fun Shop.getNumberOfDeliveredProductByCity(): Map<City, Int> {
+    val deliveredProducts = mutableMapOf<City, Int>()
+    customers.forEach { customer ->
+        val city = customer.city
+        val count = customer.orders.filter { it.isDelivered }.flatMap { it.products }.size
+        deliveredProducts[city] = deliveredProducts.getOrDefault(city, 0) + count
+    }
+    return deliveredProducts
+}
 
 // 8. Получить соответствие в мапе: город - самый популярный продукт в городе.
-fun Shop.getMostPopularProductInCity(): Map<City, Product> = emptyMap()
+fun Shop.getMostPopularProductInCity(): Map<City, Product> {
+    return customers.groupBy { it.city }.mapValues { entry ->
+        entry.value.flatMap { it.orders }.flatMap { it.products }.groupBy { it }.maxByOrNull { it.value.size }!!.key
+    }
+}
 
 // 9. Получить набор товаров, которые заказывали все покупатели.
-fun Shop.getProductsOrderedByAll(): Set<Product> = emptySet()
+fun Shop.getProductsOrderedByAll(): Set<Product> {
+    return customers.associateBy(
+        { it },
+        { customer ->
+            customer.orders.flatMap { it.products }.toSet()
+        }).values.reduce { accumulator: Set<Product>, products -> accumulator.intersect(products) }
+}
 
