92095367ed85eb57c42bc51d3e0f0c6a8a6062a5
Alexander Gagarin <sacred@xaker.ru>
2023-11-16 21:40:50 +0300

    Gagarin Homework #4

--- serialization/src/main/kotlin/ru/sber/serialization/Client1.kt
+++ serialization/src/main/kotlin/ru/sber/serialization/Client1.kt
@@ -1,7 +1,11 @@
 package ru.sber.serialization
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import java.time.LocalDate
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonFormat(with = [JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES])
 data class Client1(
     val firstName: String,
     val lastName: String,
--- serialization/src/main/kotlin/ru/sber/serialization/Client7.kt
+++ serialization/src/main/kotlin/ru/sber/serialization/Client7.kt
@@ -1,7 +1,26 @@
 package ru.sber.serialization
 
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+
+@JsonDeserialize(using = Client7Deserializer::class)
 data class Client7(
     val firstName: String,
     val lastName: String,
     val middleName: String,
 )
+
+class Client7Deserializer : JsonDeserializer<Client7>() {
+    override fun deserialize(parser: JsonParser, context: DeserializationContext): Client7 {
+        val node = parser.codec.readTree<JsonNode>(parser)
+        val values = node.get("client").textValue().split(' ', limit = 3)
+        val lastName = values.getOrElse(0) { "" }
+        val firstName = values.getOrElse(1) { "" }
+        val middleName = values.getOrElse(2) { "" }
+        return Client7(firstName, lastName, middleName)
+    }
+}
+
