13d67e5c67878f65eabea18d91534d44c6e5ea38
danilovae <liza@danilova.me>
2023-11-18 18:57:24 +0300

    hw4

--- serialization/src/test/kotlin/ru/sber/serialization/JsonCustomNamingPropertyStrategyTest.kt
+++ serialization/src/test/kotlin/ru/sber/serialization/JsonCustomNamingPropertyStrategyTest.kt
@@ -1,6 +1,10 @@
 package ru.sber.serialization
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
@@ -8,12 +12,18 @@ import kotlin.test.assertEquals
 
 class JsonCustomNamingPropertyStrategyTest {
 
+    companion object UpperCaseCustomStrategy: PropertyNamingStrategies.NamingBase() {
+        override fun translate(p0: String?): String = p0?.uppercase() ?: ""
+    }
+
     @Test
     fun `Кастомная стратегия десериализации`() {
         // given
         val data =
             """{"FIRSTNAME": "Иван", "LASTNAME": "Иванов", "MIDDLENAME": "Иванович", "PASSPORTNUMBER": "123456", "PASSPORTSERIAL": "1234", "BIRTHDATE": "1990-01-01"}"""
         val objectMapper = ObjectMapper()
+            .registerModules(KotlinModule(), JavaTimeModule())
+            .setPropertyNamingStrategy(UpperCaseCustomStrategy)
 
         // when
         val client = objectMapper.readValue<Client1>(data)
--- streams/src/main/kotlin/ru/sber/streams/Streams.kt
+++ streams/src/main/kotlin/ru/sber/streams/Streams.kt
@@ -3,32 +3,75 @@ package ru.sber.streams
 
 // 1. Используя withIndex() посчитать сумму элементов листа, индекс которых кратен 3. (нулевой индекс тоже входит)
 fun getSumWithIndexDivisibleByThree(list: List<Long>): Long {
-    return 0
+    return list.withIndex().filter { it.index % 3 == 0 }.sumOf { it.value }
 }
 
-// 2. Используя функцию generateSequence() создать последовательность, возвращающую числа Фибоначчи.
+// 2. Используя функцию generateSequence() создать последовательность, возвращающую числа Фибоначчи.  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377...
 fun generateFibonacciSequence(): Sequence<Int> {
-    return emptySequence()
+    return generateSequence(Pair(0, 1), { Pair(it.second, it.first + it.second) }).map { it.first }
 }
 
 // 3. Получить города, в которых есть покупатели.
-fun Shop.getCustomersCities(): Set<City> = emptySet()
+fun Shop.getCustomersCities(): Set<City> = customers.map { it.city }.toSet()
 
 // 4. Получить все когда-либо заказанные продукты.
-fun Shop.allOrderedProducts(): Set<Product> = emptySet()
+fun Shop.allOrderedProducts(): Set<Product> = customers.flatMap { it.orders.flatMap { it.products } }.toSet()
 
 // 5. Получить покупателя, который сделал больше всего заказов.
-fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? = null
+fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? = customers.maxWithOrNull(Comparator.comparing { it.orders.size })
 
 // 6. Получить самый дорогой продукт, когда-либо приобртенный покупателем.
-fun Customer.getMostExpensiveProduct(): Product? = null
+fun Customer.getMostExpensiveProduct(): Product? = orders.flatMap { it.products }.maxWithOrNull(Comparator.comparing { it.price })
 
 // 7. Получить соответствие в мапе: город - количество заказанных и доставленных продуктов в данный город.
-fun Shop.getNumberOfDeliveredProductByCity(): Map<City, Int> = emptyMap()
+fun Shop.getNumberOfDeliveredProductByCity(): Map<City, Int> {
+    // у каждого клиента соответствие города и кол-ва продуктов в доставленных заказах
+    // группируем по городу и получаем соответствие город - колво продуктов
+    // суммируем
+    return customers.map { it.city to it.orders.filter { it.isDelivered }.sumOf { it.products.size } }
+        .groupBy { it.first }
+        .map { it.key to it.value.sumOf { it.second } }.toMap()
+}
 
-// 8. Получить соответствие в мапе: город - самый популярный продукт в городе.
-fun Shop.getMostPopularProductInCity(): Map<City, Product> = emptyMap()
+// 8. Получить соответствие в мапе: город - самый популярный продукт в городе. city-product, city-product:kolvo, city-
+fun Shop.getMostPopularProductInCity(): Map<City, Product> {
+
+    // город - все товары
+    // город - товар-колво
+    // город - макс колво
+    return customers.map { it.city to it.orders.flatMap { it.products } }.groupBy( { it.first }, { it.second } )
+        .map { it.key to it.value.flatten().groupBy({ it }, { it }).map { it.key to it.value.size } }
+        .map { it.first to it.second.reduce { acc, pair -> maxOf(acc, pair, Comparator.comparing { it.second }) }.first }
+        .toMap()
+}
 
 // 9. Получить набор товаров, которые заказывали все покупатели.
-fun Shop.getProductsOrderedByAll(): Set<Product> = emptySet()
+fun Shop.getProductsOrderedByAll(): Set<Product> {
+    // покупатель - продукты
+    // продукт - set<покупатель>
+    // List<Pair>
+    return customers.map {it to it.orders.flatMap { it.products }}.flatMap { it.second.map { p -> p to it.first } }
+        .groupBy({ it.first }, {it.second}).map { it.key to it.value.toSet() }.filter { it.second.size == customers.size }
+        .map { it.first }.toSet()
+}
+
+
+data class Sales(val year: Int, val price: Int)
+
+val myList = listOf(
+    Sales(2017, 10),
+    Sales(2017, 19),
+    Sales(2020, 15),
+    Sales(2021, 100),
+    Sales(2020, 20),
+)
+
+fun main () {
+    val reduced = myList.groupBy({ it.year }, { it })
+        .mapValues { it.value.reduce{ left, right ->
+            Sales(left.year, (left.price + right.price)) } }
+        .values
+        .sortedBy { it.year }
+    reduced.forEach { println("${it.year}: ${it.price}") }
+}
 
